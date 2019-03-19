6408c0b5cd4a5d5e6f17e7e0b4e34ad9d5d895e0
aqilirsyad <48275018+aqilirsyad@users.noreply.github.com>
2019-03-20 01:34:06 +0800

    Update and rename java-client to client.java

--- client.java
+++ client.java
@@ -8,27 +8,35 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 
-public class SocketClientExample {
+class SocketClient {
 
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
        
-        Socket sock = new Socket("192.168.242.128", 6789);
+        Socket sock = new Socket("192.168.154.130", 6789);
         Socket socket = null;
         ObjectOutputStream oos = null;
         ObjectInputStream ois = null;
         for(int i=0; i<5;i++){
+            
             //establish socket connection to server
-            socket = new Socket("192.168.242.128", 9876);
+            
+            socket = new Socket("192.168.154.130", 9876);
+            
             //write to socket using ObjectOutputStream
+            
             oos = new ObjectOutputStream(socket.getOutputStream());
             System.out.println("Sending request to Socket Server");
             if(i==4)oos.writeObject("exit");
             else oos.writeObject(""+i);
+            
             //read the server response message
+            
             ois = new ObjectInputStream(socket.getInputStream());
             String message = (String) ois.readObject();
             System.out.println("Message: " + message);
+            
             //close resources
+            
             ois.close();
             oos.close();
             Thread.sleep(100);
